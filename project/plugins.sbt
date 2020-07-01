843e1660865f33dc90113568cd6e1a0bc3fdfac3
zeta <jzelayeta@tiendanube.com>
2020-07-01 12:28:12 -0300

    java version

--- project/plugins.sbt
+++
@@ -1,2 +0,0 @@
-addSbtPlugin("com.artima.supersafe" % "sbtplugin" % "1.1.3")
-addSbtPlugin("de.heikoseeberger" % "sbt-groll" % "6.1.0")
---
+++ src/test/java/TelephoneBillingTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TelephoneBillingTest {
+
+    @Test
+    public void dymmyTest(){
+        assertEquals(1,1);
+    }
+
+}
